142c80ee2ca6e35860d7e5c37b9aa2d2894965a8
snakesgun <snakesgun@gmail.com>
2022-03-05 15:52:02 +0800

    change toc style for mobile

--- assets/js/even.js
+++ assets/js/even.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 const Even = {};
 
-Even.backToTop = function() {
-  const $backToTop = $('#back-to-top');
+Even.backToTop = function () {
+  const $backToTop = $("#back-to-top");
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
       $backToTop.fadeIn(1000);
     } else {
@@ -13,45 +13,45 @@ Even.backToTop = function() {
     }
   });
 
-  $backToTop.click(function() {
-    $('body,html').animate({scrollTop: 0});
+  $backToTop.click(function () {
+    $("body,html").animate({ scrollTop: 0 });
   });
 };
 
-Even.mobileNavbar = function() {
-  const $mobileNav = $('#mobile-navbar');
-  const $mobileNavIcon = $('.mobile-navbar-icon');
+Even.mobileNavbar = function () {
+  const $mobileNav = $("#mobile-navbar");
+  const $mobileNavIcon = $(".mobile-navbar-icon");
   const slideout = new Slideout({
-    'panel': document.getElementById('mobile-panel'),
-    'menu': document.getElementById('mobile-menu'),
-    'padding': 180,
-    'tolerance': 70,
+    panel: document.getElementById("mobile-panel"),
+    menu: document.getElementById("mobile-menu"),
+    padding: 180,
+    tolerance: 70,
   });
   slideout.disableTouch();
 
-  $mobileNavIcon.click(function() {
+  $mobileNavIcon.click(function () {
     slideout.toggle();
   });
 
-  slideout.on('beforeopen', function() {
-    $mobileNav.addClass('fixed-open');
-    $mobileNavIcon.addClass('icon-click').removeClass('icon-out');
+  slideout.on("beforeopen", function () {
+    $mobileNav.addClass("fixed-open");
+    $mobileNavIcon.addClass("icon-click").removeClass("icon-out");
   });
 
-  slideout.on('beforeclose', function() {
-    $mobileNav.removeClass('fixed-open');
-    $mobileNavIcon.addClass('icon-out').removeClass('icon-click');
+  slideout.on("beforeclose", function () {
+    $mobileNav.removeClass("fixed-open");
+    $mobileNavIcon.addClass("icon-out").removeClass("icon-click");
   });
 
-  $('#mobile-panel').on('touchend', function() {
+  $("#mobile-panel").on("touchend", function () {
     slideout.isOpen() && $mobileNavIcon.click();
   });
 };
 
-Even._initToc = function() {
+Even._initToc = function () {
   const SPACING = 20;
-  const $toc = $('.post-toc');
-  const $footer = $('.post-footer');
+  const $toc = $(".post-toc");
+  const $footer = $(".post-footer");
 
   if ($toc.length) {
     const minScrollTop = $toc.offset().top - SPACING;
@@ -59,20 +59,23 @@ Even._initToc = function() {
 
     const tocState = {
       start: {
-        'position': 'absolute',
-        'top': minScrollTop,
+        position: "absolute",
+        top: minScrollTop,
       },
       process: {
-        'position': 'fixed',
-        'top': SPACING,
+        position: "fixed",
+        top: SPACING,
+      },
+      process_mobile: {
+        top: SPACING,
       },
       end: {
-        'position': 'absolute',
-        'top': maxScrollTop,
+        position: "absolute",
+        top: maxScrollTop,
       },
     };
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       const scrollTop = $(window).scrollTop();
 
       if (scrollTop < minScrollTop) {
@@ -80,25 +83,29 @@ Even._initToc = function() {
       } else if (scrollTop > maxScrollTop) {
         $toc.css(tocState.end);
       } else {
-        $toc.css(tocState.process);
+        if ($(document).width() > 768) {
+          $toc.css(tocState.process);
+        } else {
+          $toc.css(tocState.process_mobile);
+        }
       }
     });
   }
 
   const HEADERFIX = 30;
-  const $toclink = $('.toc-link');
-  const $headerlink = $('.headerlink');
-  const $tocLinkLis = $('.post-toc-content li');
+  const $toclink = $(".toc-link");
+  const $headerlink = $(".headerlink");
+  const $tocLinkLis = $(".post-toc-content li");
 
-  const headerlinkTop = $.map($headerlink, function(link) {
+  const headerlinkTop = $.map($headerlink, function (link) {
     return $(link).offset().top;
   });
 
-  const headerLinksOffsetForSearch = $.map(headerlinkTop, function(offset) {
+  const headerLinksOffsetForSearch = $.map(headerlinkTop, function (offset) {
     return offset - HEADERFIX;
   });
 
-  const searchActiveTocIndex = function(array, target) {
+  const searchActiveTocIndex = function (array, target) {
     for (let i = 0; i < array.length - 1; i++) {
       if (target > array[i] && target <= array[i + 1]) return i;
     }
@@ -106,60 +113,67 @@ Even._initToc = function() {
     return -1;
   };
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     const scrollTop = $(window).scrollTop();
-    const activeTocIndex = searchActiveTocIndex(headerLinksOffsetForSearch, scrollTop);
+    const activeTocIndex = searchActiveTocIndex(
+      headerLinksOffsetForSearch,
+      scrollTop
+    );
 
-    $($toclink).removeClass('active');
-    $($tocLinkLis).removeClass('has-active');
+    $($toclink).removeClass("active");
+    $($tocLinkLis).removeClass("has-active");
 
     if (activeTocIndex !== -1 && $toclink[activeTocIndex] != null) {
-      $($toclink[activeTocIndex]).addClass('active');
+      $($toclink[activeTocIndex]).addClass("active");
       let ancestor = $toclink[activeTocIndex].parentNode;
-      while (ancestor.tagName !== 'NAV') {
-        $(ancestor).addClass('has-active');
+      while (ancestor.tagName !== "NAV") {
+        $(ancestor).addClass("has-active");
         ancestor = ancestor.parentNode.parentNode;
       }
     }
   });
 };
 
-Even.fancybox = function() {
+Even.fancybox = function () {
   if ($.fancybox) {
-    $('.post-content').each(function() {
-      $(this).find('img').each(function() {
-        $(this).wrap(`<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`);
-      });
+    $(".post-content").each(function () {
+      $(this)
+        .find("img")
+        .each(function () {
+          $(this).wrap(
+            `<a class="fancybox" href="${this.src}" data-fancybox="gallery" data-caption="${this.title}"></a>`
+          );
+        });
     });
 
-    $('.fancybox').fancybox({
-      selector: '.fancybox',
+    $(".fancybox").fancybox({
+      selector: ".fancybox",
       protect: true,
     });
   }
 };
 
-Even.highlight = function() {
-  const blocks = document.querySelectorAll('pre code');
+Even.highlight = function () {
+  const blocks = document.querySelectorAll("pre code");
   for (let i = 0; i < blocks.length; i++) {
     const block = blocks[i];
     const rootElement = block.parentElement;
     const lineCodes = block.innerHTML.split(/\n/);
-    if (lineCodes[lineCodes.length - 1] === '') lineCodes.pop();
+    if (lineCodes[lineCodes.length - 1] === "") lineCodes.pop();
     const lineLength = lineCodes.length;
 
-    let codeLineHtml = '';
+    let codeLineHtml = "";
     for (let i = 0; i < lineLength; i++) {
       codeLineHtml += `<div class="line">${i + 1}</div>`;
     }
 
-    let codeHtml = '';
+    let codeHtml = "";
     for (let i = 0; i < lineLength; i++) {
       codeHtml += `<div class="line">${lineCodes[i]}</div>`;
     }
 
-    block.className += ' highlight';
-    const figure = document.createElement('figure');
+    block.className += " highlight";
+    const figure = document.createElement("figure");
     figure.className = block.className;
     figure.innerHTML = `<table><tbody><tr><td class="gutter"><pre>${codeLineHtml}</pre></td><td class="code"><pre>${codeHtml}</pre></td></tr></tbody></table>`;
 
@@ -167,20 +181,20 @@ Even.highlight = function() {
   }
 };
 
-Even.chroma = function() {
-  const blocks = document.querySelectorAll('.highlight > .chroma');
+Even.chroma = function () {
+  const blocks = document.querySelectorAll(".highlight > .chroma");
   for (let i = 0; i < blocks.length; i++) {
     const block = blocks[i];
-    const afterHighLight = block.querySelector('pre.chroma > code[data-lang]');
-    const lang = afterHighLight ? afterHighLight.className : '';
-    block.className += ' ' + lang;
+    const afterHighLight = block.querySelector("pre.chroma > code[data-lang]");
+    const lang = afterHighLight ? afterHighLight.className : "";
+    block.className += " " + lang;
   }
 };
 
-Even.toc = function() {
-  const tocContainer = document.getElementById('post-toc');
+Even.toc = function () {
+  const tocContainer = document.getElementById("post-toc");
   if (tocContainer !== null) {
-    const toc = document.getElementById('TableOfContents');
+    const toc = document.getElementById("TableOfContents");
     if (toc === null) {
       // toc = true, but there are no headings
       tocContainer.parentNode.removeChild(tocContainer);
@@ -192,25 +206,27 @@ Even.toc = function() {
   }
 };
 
-Even._refactorToc = function(toc) {
+Even._refactorToc = function (toc) {
   // when headings do not start with `h1`
   const oldTocList = toc.children[0];
   let newTocList = oldTocList;
   let temp;
-  while (newTocList.children.length === 1
-      && (temp = newTocList.children[0].children[0]).tagName === 'UL') {
+  while (
+    newTocList.children.length === 1 &&
+    (temp = newTocList.children[0].children[0]).tagName === "UL"
+  ) {
     newTocList = temp;
   }
 
   if (newTocList !== oldTocList) toc.replaceChild(newTocList, oldTocList);
 };
 
-Even._linkToc = function() {
-  const links = document.querySelectorAll('#TableOfContents a:first-child');
-  for (let i = 0; i < links.length; i++) links[i].className += ' toc-link';
+Even._linkToc = function () {
+  const links = document.querySelectorAll("#TableOfContents a:first-child");
+  for (let i = 0; i < links.length; i++) links[i].className += " toc-link";
 
   for (let num = 1; num <= 6; num++) {
-    const headers = document.querySelectorAll('.post-content>h' + num);
+    const headers = document.querySelectorAll(".post-content>h" + num);
     for (let i = 0; i < headers.length; i++) {
       const header = headers[i];
       header.innerHTML = `<a href="#${header.id}" class="headerlink anchor"><i class="iconfont icon-link"></i></a>${header.innerHTML}`;
@@ -218,62 +234,71 @@ Even._linkToc = function() {
   }
 };
 
-Even.flowchart = function() {
+Even.flowchart = function () {
   if (!window.flowchart) return;
 
-  const blocks = document.querySelectorAll('pre code.language-flowchart, pre code.language-flow');
+  const blocks = document.querySelectorAll(
+    "pre code.language-flowchart, pre code.language-flow"
+  );
   for (let i = 0; i < blocks.length; i++) {
     if (!window.hljs && i % 2 === 0) continue;
 
     const block = blocks[i];
     const rootElement = window.hljs
-        ? block.parentElement
-        : block.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+      ? block.parentElement
+      : block.parentElement.parentElement.parentElement.parentElement
+          .parentElement.parentElement.parentElement;
 
-    const container = document.createElement('div');
+    const container = document.createElement("div");
     const id = `js-flowchart-diagrams-${i}`;
     container.id = id;
-    container.className = 'align-center';
+    container.className = "align-center";
     rootElement.parentElement.replaceChild(container, rootElement);
 
     const diagram = flowchart.parse(block.childNodes[0].nodeValue);
-    diagram.drawSVG(id, window.flowchartDiagramsOptions ? window.flowchartDiagramsOptions : {});
+    diagram.drawSVG(
+      id,
+      window.flowchartDiagramsOptions ? window.flowchartDiagramsOptions : {}
+    );
   }
 };
 
-Even.sequence = function() {
+Even.sequence = function () {
   if (!window.Diagram) return;
 
-  const blocks = document.querySelectorAll('pre code.language-sequence');
+  const blocks = document.querySelectorAll("pre code.language-sequence");
   for (let i = 0; i < blocks.length; i++) {
     if (!window.hljs && i % 2 === 0) continue;
 
     const block = blocks[i];
     const rootElement = window.hljs
-        ? block.parentElement
-        : block.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+      ? block.parentElement
+      : block.parentElement.parentElement.parentElement.parentElement
+          .parentElement.parentElement.parentElement;
 
-    const container = document.createElement('div');
+    const container = document.createElement("div");
     const id = `js-sequence-diagrams-${i}`;
     container.id = id;
-    container.className = 'align-center';
+    container.className = "align-center";
     rootElement.parentElement.replaceChild(container, rootElement);
 
     const diagram = Diagram.parse(block.childNodes[0].nodeValue);
-    diagram.drawSVG(id, window.sequenceDiagramsOptions
+    diagram.drawSVG(
+      id,
+      window.sequenceDiagramsOptions
         ? window.sequenceDiagramsOptions
-        : {theme: 'simple'});
+        : { theme: "simple" }
+    );
   }
 };
 
-Even.responsiveTable = function() {
-  const tables = document.querySelectorAll('.post-content table:not(.lntable)');
+Even.responsiveTable = function () {
+  const tables = document.querySelectorAll(".post-content table:not(.lntable)");
   for (let i = 0; i < tables.length; i++) {
     const table = tables[i];
-    const wrapper = document.createElement('div');
-    wrapper.className = 'table-wrapper';
+    const wrapper = document.createElement("div");
+    wrapper.className = "table-wrapper";
     table.parentElement.replaceChild(wrapper, table);
     wrapper.appendChild(table);
   }
 };
-
--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,67 @@ if (window.hljs) {
   Even.chroma();
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("init_toc");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("toc-close");
+var body = document.querySelector("body");
+
+// var toc_link = Array.from(document.getElementsByClassName("toc-link"));
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  body.style.overflow = "hidden";
+  // openNav();
+  var toc_link = Array.from(document.getElementsByClassName("toc-link"));
+  // console.log(toc_link)
+  toc_link.forEach((ele) => {
+    // console.log(ele);
+    ele.onclick = (e) => {
+      // console.log("hello");
+      modal.style.display = "none";
+      body.style.overflow = "auto";
+      // closeNav();
+    };
+  });
+};
+
+// toc_link.forEach(
+//   ele => {
+//     ele.onclick = e => {
+//       console.log("hello");
+//       modal.style.display = "none";
+//     }
+//   }
+// )
+
+// toc_link.onclick = function() {
+//   console.log('hello')
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  body.style.overflow = "auto";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    body.style.overflow = "auto";
+  }
+};
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
